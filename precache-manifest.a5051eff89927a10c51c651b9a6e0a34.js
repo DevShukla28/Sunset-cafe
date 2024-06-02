f135f3970ad6df5ed5edfe06184526790d4a0552
Devendra pratap shukla <devendrapratapshukla0052@gmail.com>
2024-06-02 10:48:33 +0530

    Updates

---
+++ precache-manifest.a5051eff89927a10c51c651b9a6e0a34.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4639ee480f167a543f9bc89ce127ae81",
+    "url": "/Sunset-cafe/index.html"
+  },
+  {
+    "revision": "e9e52d75891c31cbda9f",
+    "url": "/Sunset-cafe/static/css/2.77f4bac1.chunk.css"
+  },
+  {
+    "revision": "cfc969d1ed306a520bc5",
+    "url": "/Sunset-cafe/static/css/main.358137a7.chunk.css"
+  },
+  {
+    "revision": "e9e52d75891c31cbda9f",
+    "url": "/Sunset-cafe/static/js/2.28e1e907.chunk.js"
+  },
+  {
+    "revision": "cfc969d1ed306a520bc5",
+    "url": "/Sunset-cafe/static/js/main.cedc52de.chunk.js"
+  },
+  {
+    "revision": "0f4f0be2359b7d6b389b",
+    "url": "/Sunset-cafe/static/js/runtime~main.d71191c1.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Sunset-cafe/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Sunset-cafe/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Sunset-cafe/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Sunset-cafe/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Sunset-cafe/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Sunset-cafe/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Sunset-cafe/static/media/slick.f97e3bbf.svg"
+  }
+]);
